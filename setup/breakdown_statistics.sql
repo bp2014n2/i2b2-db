5f6be8f28f59b93cac1e155df6f8f16abda62d97
Philipp Schirmer <philipp@schirmer-online.de>
2015-05-18 19:01:58 +0200

    Update breakdown_statistics.sql

--- setup/breakdown_statistics.sql
+++ setup/breakdown_statistics.sql
@@ -1,3 +1,4 @@
-﻿UPDATE i2b2demodata.qt_breakdown_path SET value = '\\EVA_META_STAMMDATEN\Stammdaten\Alter\' WHERE name = 'PATIENT_AGE_COUNT_XML'
-UPDATE i2b2demodata.qt_breakdown_path SET value = '\\EVA_META_STAMMDATEN\Stammdaten\Sex\' WHERE name = 'PATIENT_GENDER_COUNT_XML'
-DELETE FROM i2b2demodata.qt_query_result_type WHERE name = 'PATIENT_RACE_COUNT_XML'
+UPDATE i2b2demodata.qt_breakdown_path SET value = '\\EVA_META_STAMMDATEN\Stammdaten\Alter\' WHERE name = 'PATIENT_AGE_COUNT_XML';
+UPDATE i2b2demodata.qt_breakdown_path SET value = '\\EVA_META_STAMMDATEN\Stammdaten\Sex\' WHERE name = 'PATIENT_GENDER_COUNT_XML';
+DELETE FROM i2b2demodata.qt_query_result_instance WHERE result_type_id IN (SELECT result_type_id FROM i2b2demodata.qt_query_result_type WHERE name = 'PATIENT_RACE_COUNT_XML');
+DELETE FROM i2b2demodata.qt_query_result_type WHERE name = 'PATIENT_RACE_COUNT_XML';
