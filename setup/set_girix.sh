c0d3a04eaa7a57641c232767d1e6a35b2bf12468
Philipp Schirmer <philipp.schirmer@student.hpi.uni-potsdam.de>
2015-05-28 21:03:06 +0200

    added girix util script

---
+++ setup/set_girix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+GIRIX_LOC = localhost:9090
+
+if [ $# -ge 1 ]
+then
+    GIRIX_LOC=$1
+fi
+
+sudo -u postgres psql -d i2b2 -c "UPDATE i2b2pm.pm_cell_data SET url='http://`echo $GIRIX_LOC`/i2b2/services/GIRIXService/' WHERE cell_id LIKE 'GIRIX'"
